c36db29f786cb674fbbb268c9021a4361ae478e2
tullyelly <12563331+tullyelly@users.noreply.github.com>
2025-09-27 10:44:26 -0500

    one more testing attempt [WU-378]

--- lib/env.ts
+++ lib/env.ts
@@ -4,4 +4,7 @@ const Env = z.object({
   DATABASE_URL: z.string().url(),
 });
 
-export const { DATABASE_URL } = Env.parse(process.env);
+const rawDatabaseUrl =
+  process.env.DATABASE_URL ?? process.env.TEST_DATABASE_URL ?? undefined;
+
+export const { DATABASE_URL } = Env.parse({ DATABASE_URL: rawDatabaseUrl });
