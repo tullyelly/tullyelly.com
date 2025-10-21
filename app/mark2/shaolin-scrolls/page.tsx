926ed3fbbdc2844a3ef8978580f35084984e6c00
tullyelly <12563331+tullyelly@users.noreply.github.com>
2025-10-21 14:48:33 -0500

    add some background on shaolin-scrolls [WU-477]

--- app/mark2/shaolin-scrolls/page.tsx
+++ app/mark2/shaolin-scrolls/page.tsx
@@ -86,6 +86,22 @@ export default async function Page({ searchParams }: PageProps) {
           Shaolin Scrolls
         </h1>
       </header>
+      <section className="space-y-3">
+        <p className="text-[16px] md:text-[18px] text-muted-foreground">
+          The shaolin scrolls app was created to help me manage all of my
+          personal projects through one release system. This has allowed me to
+          bucket work into my alter egos and continue to release card sorting,
+          documentation, development, storytelling, and any number of other
+          things on a relatively predictable schedule.
+        </p>
+        <p className="text-[16px] md:text-[18px] text-muted-foreground">
+          The major.minor.patch structure I landed on is flexible and suits my
+          needs. By pushing my work through this structure, it helps me to
+          consider when things will get done, and more importantly, is the work
+          completed or not. Sometimes you need to close the book on an idea that
+          is good enough and this helps me do just that.
+        </p>
+      </section>
       <section className="space-y-6">
         <ActionBar q={q} />
         <ScrollsPageClient rows={items} meta={meta} />
