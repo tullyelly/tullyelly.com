50a8078ec289bea7c52a130ca1aa74100954fb25
tullyelly <12563331+tullyelly@users.noreply.github.com>
2025-12-07 11:30:31 -0600

    most used tags card created [WU-585]

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { BlogBoiCard } from "@/components/home/blog-boi-card";
 import { AlterEgoCard } from "@/components/home/cards/alter-ego-card";
+import { TopChronicleTagsCard } from "@/components/home/top-chronicle-tags-card";
 
 export default async function HomePage() {
   return (
     <div className="grid gap-6 p-6 md:grid-cols-2 xl:grid-cols-3">
       <AlterEgoCard />
       <BlogBoiCard />
+      <TopChronicleTagsCard />
     </div>
   );
 }
